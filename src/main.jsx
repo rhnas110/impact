96bd60387f617be5d7a6045f9e29bf9898715775
rhnas110 <raihanas110@gmail.com>
2024-06-04 02:01:22 +0700

    fix: remove package react-scroll-parallax

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { ParallaxProvider } from "react-scroll-parallax";
 
 import App from "./App.jsx";
 import "./index.css";
@@ -11,9 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
       <Router>
-        <ParallaxProvider>
-          <App />
-        </ParallaxProvider>
+        <App />
       </Router>
     </HelmetProvider>
   </React.StrictMode>
